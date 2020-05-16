0922cc34ca48e0c22ae46d40cb2525194b1b4863
Andre Vallestero <andrevallestero@gmail.com>
2020-05-16 17:17:53 -0400

    Added matches and api-match endpoints

--- tinder_swiper.py
+++ tinder_swiper.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
-from os import path
+from os import path, rename
+from pathlib import Path
+from openface_api.wrapper import process_pics
 
 STATIC_FOLDER = 'static/'
-IMAGES_FOLDER = 'images/'
+BASE_FOLDER = 'base/'
+PROCESSED_FOLDER = 'processed/'
 
 app = Flask(__name__)
 app._static_folder = STATIC_FOLDER
@@ -13,13 +16,39 @@ def root():
 
 # Respond with token that the user can use to acess a preview of the profiles in real time
 #   Token is a hash of the image sent (sha1? murmur2?)
-@app.route('/upload', methods=['POST'])
-def upload():
+@app.route('/matches', methods=['GET'])
+def matches():
     file = request.files['file']
     if file and '.' in file.filename:
         # Create a hash and save it
-        filename = str(hash(file) + file.filename.rsplit('.', 1)[1]
-        file.save(IMAGES_FOLDER + filename)
+        token = str(hash(file))
+        filename = token + '.' + file.filename.rsplit('.', 1)[1]
+        Path(BASE_FOLDER).mkdir(parents=True, exist_ok=True)
+        filepath = BASE_FOLDER + filename
+        file.save(filepath)
+
+        # Create a base image
+        process_pics(filepath)
+
+        # Move the csv into base
+        processed_file = PROCESSED_FOLDER + token + 'csv'
+        os.rename(PROCESSED_FOLDER + processed_file, BASE_FOLDER + processed_file)
+                
+        return app.send_static_file('markup/matches.html')
+    else:
+        return 'Image not recieved', 400
+
+@app.route('/api/match', methods=['GET'])
+def match():
+    token = request.args['token']
+    if token:
+        pass
+        # Get profile and pictures from tinder api
+        # Store pictures in /comparison/token/token{0-X}.jpg
+        # Run open face in whole dir
+        # Swipe right or left
+        # Send best picture in comparison dir
+        # For picture in comparison dir, delete picture and corresponding pic in processed
     
 
 if __name__ == "__main__":
