d5852dda48a1f66747d37cdeaa81c9db9716c813
Julie Nguyen <julie-nguyen@live.com>
2020-05-15 23:30:06 -0400

    fixed drag and drop style

--- client/static/scripts/drag_and_drop.js
+++ client/static/scripts/drag_and_drop.js
@@ -1,40 +1,36 @@
 let dropArea = document.getElementById('drop-area');
 
+
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, preventDefaults, false)
-})
+  dropArea.addEventListener(eventName, preventDefaults, false);
+});
 
 function preventDefaults (e) {
-  e.preventDefault()
-  e.stopPropagation()
-}
+  e.preventDefault();
+  e.stopPropagation();
+};
 
 ['dragenter', 'dragover'].forEach(eventName => {
   dropArea.addEventListener(eventName, highlight, false)
-})
+});
 
 ['dragleave', 'drop'].forEach(eventName => {
   dropArea.addEventListener(eventName, unhighlight, false)
-})
+});
 
 function highlight(e) {
   dropArea.classList.add('highlight')
-}
+};
 
 function unhighlight(e) {
   dropArea.classList.remove('highlight')
-}
+};
 
-dropArea.addEventListener('drop', handleDrop, false)
+dropArea.addEventListener('drop', handleDrop, false);
 
-// function handleDrop(e) {
-//   let dt = e.dataTransfer
-//   let files = dt.files
+function handleDrop(e) {
+  let dt = e.dataTransfer
+  let files = dt.files
 
-//   handleFiles(files)
-// }
-
-  // dropArea.addEventListener('dragenter', handlerFunction, false)
-  // dropArea.addEventListener('dragleave', handlerFunction, false)
-  // dropArea.addEventListener('dragover', handlerFunction, false)
-  // dropArea.addEventListener('drop', handlerFunction, false)
+  // handleFiles(files)
+};
--- client/tinder_swiper.py
+++ client/tinder_swiper.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 app._static_folder = 'static'
 
 
-
+# main route
 @app.route('/')
 def hello_world():
     return render_template('tinder_swiper.html')
