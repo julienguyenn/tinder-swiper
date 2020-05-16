1217dbfda8a8bca01b2ac5591efa998fc634d7d6
Andre Vallestero <andrevallestero@gmail.com>
2020-05-16 18:31:35 -0400

    Updated dependencies

--- tinder_swiper.py
+++ tinder_swiper.py
@@ -1,6 +1,6 @@
 '''
 python -m pip install -U pip
-python -m pip install -U flask python-dateutil wget
+python -m pip install -U flask python-dateutil wget pandas numpy
 '''
 
 from flask import Flask, request
