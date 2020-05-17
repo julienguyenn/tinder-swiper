5ea1854e8a47115ea93836d4eb84e89da041be19
wvan1230 <winstonvan@live.ca>
2020-05-17 17:07:39 -0400

    added a parameter to produce tracked image

--- openface_api/wrapper.py
+++ openface_api/wrapper.py
@@ -1,8 +1,11 @@
 import subprocess
 
 
-def process_pics(in_file="", in_dir="", verbose=True) -> None:
-    args = ['OpenFace_2.2.0_win_x64\FaceLandmarkImg.exe', '-2Dfp']
+def process_pics(in_file="", in_dir="", verbose=False, csv_only=True) -> None:
+    if csv_only:
+        args = ['OpenFace_2.2.0_win_x64\FaceLandmarkImg.exe', '-2Dfp']
+    else:
+        args = ['OpenFace_2.2.0_win_x64\FaceLandmarkImg.exe']
 
     if in_file and not in_dir:
         args += ['-f', in_file]
