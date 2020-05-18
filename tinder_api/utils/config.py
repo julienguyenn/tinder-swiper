fd5e7e6e9c328ec80c4af9e155daeb8dc21de32d
Andre Vallestero <andrevallestero@gmail.com>
2020-05-17 21:51:47 -0400

    Fixed typo

--- tinder_api/utils/config.py
+++ tinder_api/utils/config.py
@@ -1,4 +1,5 @@
-tinder_token = '4a7e3917-8f54-4c65-a268-53bf4c71048a'
+# https://github.com/charliewolf/pynder
+tinder_token = ''
 
 # it is best for you to write in the token to save yourself the file I/O
 # especially if you have python byte code off
@@ -9,7 +10,7 @@ headers = {
     'platform': 'ios',
     'content-type': 'application/json',
     'User-agent': 'Tinder/7.5.3 (iPohone; iOS 10.3.2; Scale/2.00)',
-    'X-Auth-Token': '4a7e3917-8f54-4c65-a268-53bf4c71048a',
+    'X-Auth-Token': '',
 }
 
 host = 'https://api.gotinder.com'
